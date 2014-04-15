ab2fa75a1b870f29aeefc1cbdc8cdfd41b00b9e2
Kaleb Pomeroy <kaleb.pomeroy@rackspace.com>
2014-04-15 14:55:45 -0500

    Added the phpbb addon

---
+++ card.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+
+   	$(".card").mouseover(function() {
+		if($(this).has('img').length ==1){
+			$(this).find('img').remove();
+		}else{
+       		title = $(this).html().replace(/ /g,'');
+
+			div = $("<div id='" + title + "' style='display:none;padding:3px;border:1px solid #CCC;background:white;position:absolute;float:right;z-index:1000000'><div>");
+
+			$.get("http://l5r.pomeroytx.com/cards/" + $(this).html() + ".jpg", function(data){
+				$("#" + title).append(data);
+			})
+
+			div.css('left', $(this).position().left + $(this).width());
+
+
+			div.css('top', $(this).position().top );
+			$(this).append(div);
+			div.fadeIn();
+			div.click(function(){$(this).fadeOut('fast');$(this).remove();});
+		}
+    });
+	$(".card").mouseout(function(){$(this).find('div').fadeOut('fast');$(this).find('div').remove();});
+});
